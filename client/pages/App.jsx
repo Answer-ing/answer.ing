cc822174732a882813becf5f24a9af02809b02b9
Nick Krug <n.e.krug@gmail.com>
2022-09-13 15:49:56 -0400

    boilerplate

---
+++ client/pages/App.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+
+// import '../stylesheets/style.scss';
+
+return (
+  <>
+    <h1>React is rendering!</h1>
+  </>
+);
+
+export default App;
---
+++ eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  plugins: [
+    "jest",
+  ],
+  extends: [
+    'airbnb-base',
+    'airbnb-base/whitespace',
+    'plugin:jest/recommended',
+  ],
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    jest: true,
+  },
+  rules: {
+    'arrow-parens': 'off',
+    'consistent-return': 'off',
+    'func-names': 'off',
+    'import/extensions': [0, 'ignorePackages'],
+    'no-console': 'off',
+    'no-plusplus': 'off',
+    radix: 'off',
+    'react/button-has-type': 'off',
+    'react/destructuring-assignment': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/prop-types': 'off',
+  },
+};
