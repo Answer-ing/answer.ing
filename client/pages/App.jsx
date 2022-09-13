544d6ab0a7998f0a45e51b95319f01b15560ca99
Tyler Wilson <wilsontyler95@gmail.com>
2022-09-13 19:41:20 -0400

    Added OAuth token creation and verification

--- client/pages/App.jsx
+++ client/pages/App.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
 
 // import '../stylesheets/style.scss';
-
-return (
-  <>
-    <h1>React is rendering!</h1>
-  </>
-);
+function App() {
+  return (
+    <>
+      <h1>React is rendering!</h1>
+    </>
+  );
+}
 
 export default App;
