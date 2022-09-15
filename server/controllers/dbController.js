0565d38ff2e2ae856cf4c9f2c93e790b9dbdf74b
cagoncil <crystallophobia@gmail.com>
2022-09-14 18:01:25 -0700

    commit findUser functionality

--- server/controllers/dbController.js
+++ server/controllers/dbController.js
@@ -5,27 +5,50 @@ const dbController = {};
 dbController.createUser = async (req, res, next) => {
   try {
     console.log('Creating user...');
-    const {
-      name,
-      email
-    } = req.body;
-    console.log('req.body:', req.body);
+    const { name, email } = req.body;
     const values = [name, email];
-    // const text = `INSERT INTO client (name, email) VALUES ('${name}', '${email}');`;
-    // console.log('QUERY', text);
-    // const newUser = await db.query(text);
     const text = `INSERT INTO client (name, email) VALUES ($1, $2)`;
     await db.query(text, values);
-  
-    // console.log(newUser);
-    // res.locals.newUser = newUser;
     const newUser = await db.query('SELECT * FROM client ORDER BY id DESC LIMIT 1');
-    console.log('result: ', newUser.rows);
+    console.log('Successfully registered the following user: ', newUser.rows);
     res.locals.newUser = newUser.rows;
     return next();
   } catch (err) {
     console.log('Error creating user:', err);
     return next({ log: `dbController.createUser error: ${err}`, message: 'Error found in dbController.createUser' });
+  }
+}
+
+dbController.findUserByName = async (req, res, next) => {
+  try {
+    const { name, email } = req.body;
+    const text = `SELECT * FROM client 
+    WHERE LOWER(name) LIKE LOWER('%${name}%') 
+    or LOWER(email) LIKE LOWER('%${email}%')
+    ORDER BY name`;
+    const userFound = await db.query(text);
+
+    if (name) console.log(`Successfully located the following user(s) named ${name}:`, userFound.rows);
+    else console.log(`Successfully located the following user(s) with the email '${email}':`, userFound.rows[0]);
+   
+    res.locals.userFound = userFound.rows;
+    return next();
+  } catch (err) {
+    return next({ log: `dbController.findUserByName error: ${error}`, message: 'Error found @ dbControllers.findUserByName' });
+  }
+}
+
+dbController.findUserById = async (req, res, next) => {
+  try {
+    console.log('id', req.body.id)
+    const { id } = req.body;
+    const text = `SELECT * FROM client WHERE id=${id}`;
+    const userFound = await db.query(text);
+    console.log(`Successfully located the following user with ID #${id}: `, userFound.rows);
+    res.locals.userFound = userFound.rows;
+    return next();
+  } catch (err) {
+    return next({ log: `dbController.findUserById error: ${error}`, message: 'Error found @ dbControllers.findUserById' });
   }
 }
 
